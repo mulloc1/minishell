c10d3a9c71ebf0c811aa02c3ebb484f362b21f60
jaewchoi <mx757@naver.com>
2022-06-17 21:44:30 +0900

    ft_add_env.c

--- exec/ft_add_env.c
+++ exec/ft_add_env.c
@@ -1,12 +1,75 @@
 #include "minishell.h"
+#include "libft.h"
 
-void	ft_split_env(char *str, char **name, char **value)
+static void	ft_export_error(char *name)
 {
-	int	i;
-	int	j;
+	ft_putstr_fd("bash: export: `", 2);
+	ft_putstr_fd(name, 2);
+	ft_putendl_fd("\': not a valid identifier", 2);
+}
+
+static int	ft_check_name_valid(char *name)
+{
+	int i;
+
+	if (!ft_isalpha(name[0]))
+	{
+		ft_export_error(name);
+		return (FALSE);
+	}
+	i = 0;
+	while (name[++i])
+	{
+		if (!ft_isalnum(name[i]))
+		{
+			ft_export_error(name);
+			return (FALSE);
+		}
+	}
+	return (TRUE);
+}
+
+static char	*ft_remove_quote(char *str)
+{
+	int i;
+	int	cnt;
 
 	i = -1;
-	
+	cnt = 0;
+	while (str && str[++i])
+	{
+		if (str[i] == '\'' || str[i] == "\"")
+		{
+			cnt++;
+			continue ;
+		}
+		if (!cnt)
+			continue ;
+		str[i - cnt] = str[i];
+	}
+	str[i - cnt] = str[i];
+	return (str);
+}
+
+static int	ft_split_env(char *str, char **name, char **value)
+{
+	char	**split;
+	int		result;
+
+	split = ft_split(str, '=');
+	if (!split)
+		ft_error("split malloc fail\n");
+	result = TRUE;
+	*name = ft_remove_quote(split[0]);
+	*value = ft_remove_quote(split[1]);
+	if (!ft_check_name_valid(*name))
+	{
+		free(*name);
+		free(*value);
+		result = FALSE;
+	}
+	free(split);
+	return (result);
 }
 
 void	ft_add_env(t_cmd *cmd)
@@ -19,6 +82,10 @@ void	ft_add_env(t_cmd *cmd)
 	i = 0;
 	while (cmd->argv[++i])
 	{
-		ft_split_env(cmd->argv[i], &name, &value);
+		if (ft_split_env(cmd->argv[i], &name, &value))
+		{
+			// hashtable insert
+			// key = name, value = value
+		}
 	}
 }
