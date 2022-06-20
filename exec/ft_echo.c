888c5b3f418cccef0728507699c60580689babeb
jaewchoi <mx757@naver.com>
2022-06-20 23:09:11 +0900

    ft_echo.c

--- exec/ft_echo.c
+++ exec/ft_echo.c
@@ -2,24 +2,45 @@
 #include "libft.h"
 #include <stdio.h>
 
-void	ft_print(char **str)
+static void	ft_set_fd(t_cmd *cmd)
 {
-	int	i;
-
-	i = 0;
-	while (str[i])
+	if (cmd->in_fd != STDIN)
 	{
-		// 환경변수, 명령어 확인
+		dup2(cmd->in_fd, STDIN);
+		close(cmd->in_fd);
 	}
+	if (cmd->out_fd != STDOUT)
+	{
+		dup2(cmd->out_fd, STDOUT);
+		close(cmd->out_fd);
+	}
+	else if (cmd->pipe[P_WRITE] > 0)
+		dup2(cmd->pipe[P_WRITE], STDOUT);
+	close(cmd->pipe[P_READ]);
+	close(cmd->pipe[P_WRITE]);
+}
+
+static void	ft_child_proc(t_cmd *cmd)
+{
+	// if (cmd->path) -> cmd |
+	ft_set_fd(cmd);
+	if (!cmd->argv[1])
+		printf("\n");
+	else if (!ft_strncmp(cmd->argv[1], "-n", 5))
+		printf("%s", cmd->argv[2]);
+	else
+		printf("%s\n", cmd->argv[1]);
+	exit(1);
 }
 
 void	ft_echo(t_cmd *cmd)
 {
-	if (!ft_strncmp(cmd->argv[1], "-n", 5))
-		ft_print(&(cmd->argv[2]));
-	else
-	{
-		ft_print(&(cmd->argv[1]));
-		printf("\n");
-	}
+	pid_t	pid;
+
+	pid = fork();
+	if (pid < 0)
+		ft_error("fork fail\n");
+	else if (pid == 0)
+		ft_child_proc(cmd);
+	cmd->last_pid = pid;
 }
