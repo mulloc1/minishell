8f253a5c520f62a6cc0c17b2df41758adfd79fc3
Jaemin Bae <jaebae@c8r2s12.42seoul.kr>
2022-06-23 16:28:23 +0900

    env_quote_processing2.c double_quote_processing() split commit

--- srcs/env_quote_processing2.c
+++ srcs/env_quote_processing2.c
@@ -6,7 +6,7 @@
 /*   By: jaebae <jaebae@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 15:12:18 by jaebae            #+#    #+#             */
-/*   Updated: 2022/06/23 16:18:11 by jaebae           ###   ########.fr       */
+/*   Updated: 2022/06/23 16:27:04 by jaebae           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,14 +65,36 @@ int	single_quote_processing(t_tree_node *node, int idx)
 	return (temp_idx);
 }
 
+char	*processing(char *temp, t_hashtable_data *data, int *i)
+{
+	char	*temp2;
+	int		z;
+	int		j;
+
+	temp2 = ft_calloc(ft_strlen(temp) + ft_strlen((*data).value) + 1, sizeof(char));
+	while (temp[++(*i)] != '$')
+		temp2[*i] = temp[*i];
+	z = *i;
+	j = -1;
+	while ((*data).value[++j])
+		temp2[(*i)++] = (*data).value[j];
+	while ((temp[z] == '$' || ft_isnaming(temp[z])) && temp[z])
+	{
+		if (temp[z] == '$' && (temp[z + 1] == ' ' || temp[z + 1] == '\0' || temp[z + 1] == '\"'))
+			break ;
+		z++;
+	}
+	while (temp[z])
+		temp2[(*i)++] = temp[z++];
+	return (temp2);
+}
+
 int	double_quote_processing(t_tree_node *node, int idx, t_hashtable *hashtable)
 {
 	t_hashtable_data	data;
 	char				*temp; 
 	char				*temp2;
 	int					i;
-	int					j;
-	int					z;
 
 	temp = node->data.token;
 	while (1)
@@ -81,22 +103,8 @@ int	double_quote_processing(t_tree_node *node, int idx, t_hashtable *hashtable)
 		if (!data.key)
 			break ;
 		data.value = hashtable_search(hashtable, data.key);
-		temp2 = ft_calloc(ft_strlen(temp) + ft_strlen(data.value) + 1, sizeof(char));
 		i = -1;
-		while (temp[++i] != '$')
-			temp2[i] = temp[i];
-		z = i;
-		j = -1;
-		while (data.value[++j])
-			temp2[i++] = data.value[j];
-		while ((temp[z] == '$' || ft_isnaming(temp[z])) && temp[z])
-		{
-			if (temp[z] == '$' && (temp[z + 1] == ' ' || temp[z + 1] == '\0' || temp[z + 1] == '\"'))
-				break ;
-			z++;
-		}
-		while (temp[z])
-			temp2[i++] = temp[z++];
+		temp2 = processing(temp, &data, &i);	
 		free(data.key);
 		free(temp);
 		temp = temp2;
