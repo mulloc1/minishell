57552a737f6da6ae6ebd07bf1d386d6ad7702c4d
jaewchoi <mx757@naver.com>
2022-06-23 11:49:08 +0900

    /include/hashtable.h

--- include/hashtable.h
+++ include/hashtable.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   hashtable.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaebae <jaebae@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: jaewchoi <jaewchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 11:29:50 by jaebae            #+#    #+#             */
-/*   Updated: 2022/06/13 16:57:05 by jaebae           ###   ########.fr       */
+/*   Updated: 2022/06/23 11:39:14 by jaewchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,8 @@ unsigned long long hashcode_making(char *str);
 t_list  *key_overlap_check(t_list *top, char *key);
 t_hashtable *hashtable_create();
 int hashtable_insert(t_hashtable *table, char *key, char *value);
-int hashtable_remove();
-int hashtable_delete();
+int hashtable_remove(t_hashtable *table, char *key);
+int hashtable_delete(t_hashtable *table);
 int hashtable_resizing();
 char	*hashtable_search(t_hashtable *table, char *key);
 
