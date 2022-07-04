73b06e15089aa093e5ab7e7825cecf25c4b577f6
배재민 <mulloc@baejaemin-ui-Mac-mini.local>
2022-07-04 20:21:41 +0900

    존재하지 않는 환경변수를 서치할 때 다른 환경변수의 값이 나오는 문제

--- srcs/hashtable/hashtable_utils.c
+++ srcs/hashtable/hashtable_utils.c
@@ -6,7 +6,7 @@
 /*   By: jaebae <jaebae@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 16:57:13 by jaebae            #+#    #+#             */
-/*   Updated: 2022/07/01 14:23:28 by jaebae           ###   ########.fr       */
+/*   Updated: 2022/07/04 20:20:20 by mulloc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,9 @@ t_list	*key_overlap_check(t_list *top, char *key)
 				((t_hashtable_data *)top->content)->key, \
 				key, ft_strlen(key) + 1))
 		top = top->next;
-	if (ft_strncmp(\
+	if (!top && ft_strncmp(\
 				((t_hashtable_data *)top->content)->key, \
 				key, ft_strlen(key) + 1))
-		top = top->next;
+		top = NULL;
 	return (top);
 }
